b5fe74e12491c564178999d3b942d5d93dd2330e
aleks skudarnov <skudarnovfred@mail.ru>
2023-02-07 13:43:52 +0500

    modified test4

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -16,9 +16,11 @@ class MobileBankApiTestV4 {
       .when()
           .get("/demo/accounts")
       // Проверки
+              // код теста
       .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      .statusCode(200)
+      // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+      .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
 }
